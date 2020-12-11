a6758e3da87439a5ae53cd57bcb1d3e13ad21d35
ramimhossain12 <ramimhossain6242@gmail.com>
2020-12-11 23:57:38 +0600

    first commit

---
+++ app/src/main/java/com/example/documentscannerconvertimagetopdf/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.documentscannerconvertimagetopdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
